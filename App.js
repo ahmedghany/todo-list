34ce695d39018199135251fd1c190de663d69f02
ahmedghany <98774424+ahmedghany@users.noreply.github.com>
2022-11-03 12:56:02 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+//importing COmpOnents
+import Form from './components/forms';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>Ahmed TODO List</h1>
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
